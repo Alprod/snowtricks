f3732bf347fcd012ce5eab79daf128d6f68a9266
Alprod <41442860+Alprod@users.noreply.github.com>
2021-04-03 04:58:10 +0200

    configuration webpack

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+console.log('Webpack encore')
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import './fonts/fonts.css';
+import './styles/style.scss';
+
+// start the Stimulus application
+import './bootstrap';
+require('bootstrap-icons/font/bootstrap-icons.css');
+
+import jquery from 'jquery';
