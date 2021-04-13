6bc49caa1d5eae9d6d12720b49076c8dc310f883
Alprod <41442860+Alprod@users.noreply.github.com>
2021-04-13 18:42:39 +0200

    Management remember me

---
+++ migrations/Version20210413153121.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210413153121 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE rememberme_token (id INT AUTO_INCREMENT NOT NULL, series VARCHAR(90) NOT NULL, value VARCHAR(90) NOT NULL, last_used DATETIME NOT NULL, class VARCHAR(150) NOT NULL, username VARCHAR(200) NOT NULL, UNIQUE INDEX UNIQ_4C4CEC2C3A10012D (series), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE rememberme_token');
+    }
+}
