89b45012a046491e647dec60eb5bdcf8fefa7368
Alprod <41442860+Alprod@users.noreply.github.com>
2021-04-15 11:43:44 +0200

    Update fixtures

--- src/DataFixtures/FigureFixtures.php
+++ src/DataFixtures/FigureFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Figure;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -13,21 +14,30 @@ class FigureFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+        for($j = 1; $j <= 6; $j++ ) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                ->setContent($faker->realText(150));
 
-        for($i = 1; $i < 15; $i++) {
-            $figure = new Figure();
-            $figure->setTitle($faker->sentence(3))
-                ->setDescription($faker->realText(200,2))
-                ->setAuthor($this->getReference('User_'.$i)->getPseudo())
-                ->setCreatedAt($faker->dateTimeBetween('-1 years'));
-            $manager->persist($figure);
-            $this->addReference('Figure_'.$i, $figure);
+            $manager->persist($category);
+
+            for($i = 1; $i < mt_rand(6, 10); $i++) {
+                $figure = new Figure();
+                $figure->setTitle($faker->sentence(3))
+                    ->setDescription($faker->realText(200,2))
+                    ->setAuthor($this->getReference('User_'.$i)->getPseudo())
+                    ->setCategory($category)
+                    ->setCreatedAt($faker->dateTimeBetween('-1 years'));
+
+                $manager->persist($figure);
+                $this->setReference('Figure_'.$i, $figure);
+            }
         }
 
         $manager->flush();
     }
 
-    public function getDependencies()
+    public function getDependencies(): array
     {
         return [
             UserFixtures::class
